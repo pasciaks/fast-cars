636e40f096e8c098aaa2542498089d72d18c99f6
Sheldon Pasciak <sheldon@pasciak.com>
2024-02-21 17:35:50 -0700

    Refactor cars

--- ParkingLotTester.java
+++ ParkingLotTester.java
@@ -4,15 +4,17 @@ public class ParkingLotTester {
 
 	public static void main(String[] args) {
 
+		// create new ParkingLotTester instance
+
+		ParkingLotTester parkingLotTester = new ParkingLotTester();
+
+		parkingLotTester.run();
+
+	}
+
+	public void run() {
+
 		ParkingLot parkingLot = new ParkingLot();
-
-		Car[] gottenCars = parkingLot.getCars();
-		System.out.println("Cars:" + gottenCars.length);
-
-		// iterate through all gottenCars
-		for (Car car : gottenCars) {
-			System.out.println(car);
-		}
 
 		// create new car instance
 		Car car1 = new Car("Ford", "Mustang", "Red", 4, 28000.00);
@@ -28,23 +30,26 @@ public class ParkingLotTester {
 
 		// NOTE: car5 isn't being added on purpose
 
-		gottenCars = parkingLot.getCars();
+		getAndShowCars(parkingLot); // show 4 initial cars
 
+		parkingLot.removeCar(car2); // should be found + removed
+		parkingLot.removeCar(car5); // will not be found and not removed
+
+		getAndShowCars(parkingLot); // now down to 3 cars
+
+		parkingLot.addCar(car5); // now add car5
+
+		getAndShowCars(parkingLot); // back up to 4 cars
+	}
+
+	public void getAndShowCars(ParkingLot parkingLot) {
+
+		Car[] gottenCars = parkingLot.getCars();
 		System.out.println("Cars:" + gottenCars.length);
+		for (Car car : gottenCars) {
+			car.displayCar();
+		}
 
-		parkingLot.showCars(gottenCars);
-
-		parkingLot.removeCar(car2); // should be removed
-
-		parkingLot.removeCar(car5); // will not be found, not removed
-
-		gottenCars = parkingLot.getCars();
-
-		parkingLot.showCars(gottenCars);
-
-		parkingLot.addCar(car5);
-		gottenCars = parkingLot.getCars();
-		parkingLot.showCars(gottenCars);
 	}
 
 }
